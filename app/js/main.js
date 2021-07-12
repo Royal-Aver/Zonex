2b9988f0ae126b9d52b31e1b6cc7be5eba4fb8c6
Yuriy Averichev <royal_aver@rambler.ru>
2021-07-12 15:17:08 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+// $(function () {
+// 	$('.slider').slick({
+// 			$('.burger, .menu a').on('click', function () {
+// 		$('.menu').toggleClass('menu--active')
+// 	});
+
+// 	$(".menu a, .logo").on("click", function (event) {
+// 		event.preventDefault();
+// 		var id = $(this).attr('href'),
+// 			top = $(id).offset().top;
+// 		$('body,html').animate({ scrollTop: top }, 1500);
+// 	});
+
+// 	$(window).on("scroll", function () {
+// 		var wn = $(window).scrollTop();
+// 		if (wn > 120) {
+// 			$(".header__top").css("background-color", "#02373f");
+// 		}
+// 		else {
+// 			$(".header__top").css("background-color", "transparent");
+// 		}
+// 	});
+
+// 	$(window).on("scroll", function () {
+// 		if ($(window).scrollTop() > 50) {
+// 			$('.header__nav').addClass('header__nav--scroll')
+// 		}
+// 		else
+// 			$('.header__nav').removeClass('header__nav--scroll')
+// 	});
+
+// 	})
+
+// });
